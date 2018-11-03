237e23853407942322b1a68e5a8528473223ab09
Delink-D <delinkdesigns@gmail.com>
2018-11-03 21:43:11 +0300

    renamed the application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'products-catalog'`, async(() => {
+  it(`should have as title 'products-catalogue'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('products-catalog');
+    expect(app.title).toEqual('products-catalogue');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to products-catalog!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to products-catalogue!');
   }));
 });
