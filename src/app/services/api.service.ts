3e8ae37996f94ca23a5614b8ab57dd84247f66a3
Delink-D <delinkdesigns@gmail.com>
2018-11-03 23:39:51 +0300

    functionality to fetch all records from the api

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,23 @@
+import { environment } from '../../environments/environment';
+
+export class ApiService {
+  constructor(parameters) {}
+
+  /**
+   * function to fetch all type objects from the api
+   * @param type the object type
+   */
+  fetchAll(type: string): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      fetch(environment.api + type)
+        .then((res: any) => {
+          console.log('Fetched:', res.length, 'objects of', type);
+          resolve(res);
+        })
+        .catch(err => {
+          console.log('Error: fetching', type);
+          reject(err);
+        });
+    });
+  }
+}
