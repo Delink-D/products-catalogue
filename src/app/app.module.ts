12fbb2f77c97fe3dfbe13b99eccc1ca6510c9811
Delink-D <delinkdesigns@gmail.com>
2018-11-03 21:20:09 +0300

    created the need components for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ReportingComponent } from './reporting/reporting.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ReportingComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
