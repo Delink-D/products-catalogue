a07afb51da550dee136fa7dceed51100326622a3
Delink-D <delinkdesigns@gmail.com>
2018-11-04 00:35:59 +0300

    added function to call fetch categories

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../models/category';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  categories: Category[];
 
-  constructor() { }
+  constructor(
+    private categoryService: CategoryService
+    ) {}
 
   ngOnInit() {
+    this.getCategories(); // get categories
+  }
+
+  /**
+   * function to fetch all the categories
+   */
+  getCategories() {
+    this.categoryService.getAllCategories()
+      .then((categories: Category[]) => {
+        console.log('CATEGORIES:', categories);
+        this.categories = categories;
+      })
+      .catch(err => {
+        console.log('FETCH ERR:', err);
+        // snackbar
+      });
   }
 
 }
