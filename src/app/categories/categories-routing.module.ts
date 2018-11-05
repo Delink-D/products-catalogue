2f2fa5b7d3f578d9275981c70d62e9e40cbc5ceb
Delink-D <delinkdesigns@gmail.com>
2018-11-05 22:07:29 +0300

    removed the editing route

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CategoriesComponent } from './categories.component';
 import { CategoryAddComponent } from './add/category-add.component';
-// import { FacilityEditComponent } from './edit/facility-edit.component';
 import { CategoryListComponent } from './list/category-list.component';
 
 @NgModule({
@@ -20,10 +19,6 @@ import { CategoryListComponent } from './list/category-list.component';
           {
             path: 'add',
             component: CategoryAddComponent,
-          },
-          {
-            path: ':id',
-            // component: FacilityEditComponent,
           }
         ]
       }
